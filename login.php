d9fe617a63f2a30c4cf3004ae088557630426bd9
Nguyen-huan <huan06949@gmail.com>
2023-06-24 00:15:36 +0700

    create Header, Footer file

--- login.php
+++ login.php
@@ -1,72 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/responsive.css">
-    <title>LIÊN HỆ | ROWAN</title>
-</head>
+<?php $pageTitle='Login/Register on ROMAN | ROMAN'; ?>
+<?php require_once 'header.php'; ?>
 <body>
-    <!-- HEADER -->
-    <section id="header">
-        <div class="header-container">
-            <a href="index.html" class="logo"><img src="./images/logo.png" alt=""></a>
-            <div class="search-box">
-                <input type="text" placeholder="Tìm Kiếm">
-                <button type="submit" class="btn-search"><i class="fa-solid fa-magnifying-glass"></i></button>
-            </div>
-            <div class="right-box">
-                <div class="login">
-                    <div><a href="login.html">ĐĂNG NHẬP</a> / <a href="login.html">ĐĂNG KÝ</a></div>
-                </div>
-                <a class="cart" onclick = "toggleCart()">
-                    <i class="fa-solid fa-cart-shopping"></i>
-                    <span id = "count_cart">0</span>
-                </a>
-            </div>
-        </div>
-    </section>
-    <!-- END HEADER -->
 
-    <!-- NAVIGATION -->
-    <section id="navigation">
-        <ul class="nav-list">
-            <li class="nav-item">
-                <i class="fa-solid fa-house"></i>
-                <a href="index.html" class="nav-link">Trang chủ</a>
-            </li>
-            <li class="nav-item">
-                <i class="fa-solid fa-door-open"></i>
-                <a href="introduction.html" class="nav-link">Giới thiệu</a>
-            </li>
-            <li class="nav-item">
-                <i class="fa-solid fa-shirt"></i>
-                <a href="product.html" class="nav-link">Sản phẩm</a>
-            </li>
-            <li class="nav-item">
-                <i class="fa-solid fa-book"></i>
-                <a href="policy.html" class="nav-link">Chính sách</a>
-            </li>
-            <li class="nav-item">
-                <i class="fa-solid fa-phone"></i>
-                <a href="contact.html" class="nav-link">Liên hệ</a>
-            </li>
-            <li class="nav-item">
-                <i class="fa-solid fa-people-group"></i>
-                <a href="https://www.facebook.com/groups/283978698958770/" class="nav-link" target="_blank">Cộng đồng</a>
-            </li>
-        </ul>
-    </section>
-    <!-- END NAVIGATION -->
 
     <!-- LOGIN / SIGN UP -->
     <section id="login">
         <div class="sub-title black">
             <h1>Tài khoản</h1>
-            <p><a href="index.html">trang chủ </a>  /  tài khoản</p>
+            <p><a href="index.php">trang chủ </a>  /  tài khoản</p>
         </div>
         <div class="login-container">
             <div class="login-box">
